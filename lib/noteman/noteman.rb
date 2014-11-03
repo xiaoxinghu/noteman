ef624340b9082dd3a8d5e57304a801cc1900ca49
Xiaoxing Hu <dawnstar.hu@gmail.com>
2014-11-03 16:49:34 +1300

    feature for searching for cheatsheets added

--- lib/noteman/noteman.rb
+++ lib/noteman/noteman.rb
@@ -5,36 +5,53 @@ class Noteman
 
 	def initialize
 		@config = read_config
-    @config['notes_folder'] ||= "~/notes"
-    @config['inbox'] ||= "inbox.md"
+		@config['path'] ||= "~/io"
+		@config['viewer'] ||= "Marked 2"
+		@config['editor'] ||= "vim"
+		@config['inbox'] ||= "inbox.md"
 		@config['default_extention'] = "md"
-    File.open(home_config, 'w') { |yf| YAML::dump(config, yf) }
+		File.open(home_config, 'w') { |yf| YAML::dump(config, yf) }
 	end
 
 	def hi
 		puts 'Hi, I am Noteman.'
 	end
 
-  def home_config
-    if Dir.respond_to?('home')
-      File.join(Dir.home, NOTEMAN_CONFIG_NAME)
-    else
-      File.join(File.expand_path("~"), NOTEMAN_CONFIG_NAME)
-    end
-  end
+	def get_cheatsheets(topic = 'all')
+		Dir.chdir(File.expand_path(@config['path']))
+		if topic == 'all'
+			Dir.glob("cheatsheet_*.#{@config['default_extention']}").each do |file|
+				view File.expand_path(file)
+			end
+		else
+			view "#{Dir.pwd}/cheatsheet_#{topic}.md"
+		end
+	end
 
-  def read_config
-    config = {}
-    dir = Dir.pwd
-    while(dir != '/')
-      if File.exists? File.join(dir, NOTEMAN_CONFIG_NAME)
-        config = YAML.load_file(File.join(dir, NOTEMAN_CONFIG_NAME)).deep_merge!(config)
-      end
-      dir = File.dirname(dir)
-    end
-    if config.empty? && File.exists?(home_config)
-      config = YAML.load_file(home_config)
-    end
-    config
-  end
+	def view(file)
+		system("open -a \"#{@config['viewer']}\" #{file}")
+	end
+
+	def home_config
+		if Dir.respond_to?('home')
+			File.join(Dir.home, NOTEMAN_CONFIG_NAME)
+		else
+			File.join(File.expand_path("~"), NOTEMAN_CONFIG_NAME)
+		end
+	end
+
+	def read_config
+		config = {}
+		dir = Dir.pwd
+		while(dir != '/')
+			if File.exists? File.join(dir, NOTEMAN_CONFIG_NAME)
+				config = YAML.load_file(File.join(dir, NOTEMAN_CONFIG_NAME)).deep_merge!(config)
+			end
+			dir = File.dirname(dir)
+		end
+		if config.empty? && File.exists?(home_config)
+			config = YAML.load_file(home_config)
+		end
+		config
+	end
 end
