760bf62b5a5016d0ae7603e7798cb8625f817149
Xiaoxing Hu <dawnstar.hu@gmail.com>
2014-11-05 16:54:17 +1300

    bug fixed

--- lib/noteman.rb
+++ lib/noteman.rb
@@ -1,6 +1,5 @@
-require 'noteman/version.rb'
-require 'noteman/*'
+require 'tempfile'
+require 'noteman/meta_parser'
 require 'noteman/base_man'
 require 'noteman/cheat_man'
 require 'noteman/capture_man'
-require 'tempfile'
