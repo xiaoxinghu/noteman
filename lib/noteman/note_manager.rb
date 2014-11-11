79b7d132af913530804335462c9533e026a4c550
Xiaoxing Hu <dawnstar.hu@gmail.com>
2014-11-12 00:37:30 +1300

    trying things

--- lib/noteman/note_manager.rb
+++ lib/noteman/note_manager.rb
@@ -1,137 +1,145 @@
 module Noteman
-  class NoteManager
-    include Config
+	class NoteManager
+		include Config
 
-    attr_accessor :notes, :result, :tags
+		attr_accessor :notes, :result, :tags
 
-    def initialize
-      @notes = []
-      Dir.chdir(File.expand_path(config['notes_in']))
-      Dir.glob("*.#{config['ends_with']}").each do |file|
-        @notes << Note.new(file)
-      end
-    end
 
-    def search
-      @result = @notes
-      self
-    end
 
-    def by_tags(tags)
-      @result.delete_if { |note| not note.with_tags? tags }
-      self
-    end
+		def initialize
+			@notes = []
+			Dir.chdir(File.expand_path(config['notes_in']))
+			Dir.glob("*.#{config['ends_with']}").each do |file|
+				@notes << Note.new(file)
+			end
+		end
 
-    def by_keywords(keywords)
-      @result.delete_if { |note| not note.contains? keywords }
-      self
-    end
+		def self.search
+			@@manager = NoteManager.new unless defined? @@manager
+			@@manager.reset_filter
+		end
 
-    def search_by_fomular(name)
-      fomular = config['fomular'][name]
-      if fomular
-        search
-        tags = fomular['tags'].split(' ')
-        keywords = fomular['keywords'].split(' ')
-        if tags
-          by_tags tags
-        end
-        if keywords
-          by_keywords keywords
-        end
-      else
-        puts "No such fomular defined #{name}."
-      end
-      self
-    end
+		def reset_filter
+			@result = []
+			@result += @notes
+			self
+		end
 
-    def capture(input, link=nil)
-      if link
-        content = "[#{input}](#{link})"
-      else
-        content = input
-      end
-      open(config['capture_to'], 'a') do |f|
-        f << "#{content}\n"
-      end
-    end
+		def by_tags(tags)
+			@result.delete_if { |note| not note.with_tags? tags }
+			self
+		end
 
-    def choose(notes)
-      if notes.length > 1
-        notes.each_with_index do |note, i|
-          puts "% 3d: %s" % [i, note.file]
-        end
-        print "> "
-        num = STDIN.gets
-        return false if num =~ /^[a-z ]*$/i
-        chosen = notes[num.to_i]
-      elsif notes.length == 1
-        chosen = notes[0]
-      else
-        chosen = false
-      end
-      chosen
-    end
+		def by_keywords(keywords)
+			@result.delete_if { |note| not note.contains? keywords }
+			self
+		end
 
-    def view(file)
-      system("open -a \"#{config['view_with']}\" #{file}")
-    end
+		def search_by_fomular(name)
+			fomular = config['fomular'][name]
+			if fomular
+				search
+				tags = fomular['tags'].split(' ')
+				keywords = fomular['keywords'].split(' ')
+				if tags
+					by_tags tags
+				end
+				if keywords
+					by_keywords keywords
+				end
+			else
+				puts "No such fomular defined #{name}."
+			end
+			self
+		end
 
-    def view_result
-      if result.length == 0
-        puts 'No matching result found.'
-      else
-        chosen = choose result
-        view chosen.file
-      end
-    end
+		def capture(input, link=nil)
+			if link
+				content = "[#{input}](#{link})"
+			else
+				content = input
+			end
+			open(config['capture_to'], 'a') do |f|
+				f << "#{content}\n"
+			end
+		end
 
-    def tags
-      if not defined? @tags
-        @tags = {}
-        notes.each do |note|
-          note.tags.each do |tag|
-            if not @tags.has_key? tag
-              @tags[tag] = 1
-            else
-              @tags[tag] += 1
-            end
-          end
-        end
-      end
-      @tags
-    end
+		def choose(notes)
+			if notes.length > 1
+				notes.each_with_index do |note, i|
+					puts "% 3d: %s" % [i, note.file]
+				end
+				print "> "
+				num = STDIN.gets
+				return false if num =~ /^[a-z ]*$/i
+				chosen = notes[num.to_i]
+			elsif notes.length == 1
+				chosen = notes[0]
+			else
+				chosen = false
+			end
+			chosen
+		end
 
-    def fork_editor(input="")
-      tmpfile = Tempfile.new('note')
+		def view(file)
+			system("open -a \"#{config['view_with']}\" #{file}")
+		end
 
-      File.open(tmpfile.path,'w+') do |f|
-        f.puts input
-      end
+		def view_result
+			if result.length == 0
+				puts 'No matching result found.'
+			else
+				chosen = choose result
+				view chosen.file
+			end
+		end
 
-      pid = Process.fork { system("$EDITOR #{tmpfile.path}") }
+		def tags
+			if not defined? @tags
+				@tags = {}
+				notes.each do |note|
+					note.tags.each do |tag|
+						if not @tags.has_key? tag
+							@tags[tag] = 1
+						else
+							@tags[tag] += 1
+						end
+					end
+				end
+			end
+			@tags
+		end
 
-      trap("INT") {
-        Process.kill(9, pid) rescue Errno::ESRCH
-        tmpfile.unlink
-        tmpfile.close!
-        exit 0
-      }
+		def fork_editor(input="")
+			tmpfile = Tempfile.new('note')
 
-      Process.wait(pid)
+			File.open(tmpfile.path,'w+') do |f|
+				f.puts input
+			end
 
-      begin
-        if $?.exitstatus == 0
-          input = IO.read(tmpfile.path)
-        else
-          raise "Cancelled"
-        end
-      ensure
-        tmpfile.close
-        tmpfile.unlink
-      end
+			pid = Process.fork { system("$EDITOR #{tmpfile.path}") }
 
-      input
-    end
-  end
+			trap("INT") {
+				Process.kill(9, pid) rescue Errno::ESRCH
+				tmpfile.unlink
+				tmpfile.close!
+				exit 0
+			}
+
+			Process.wait(pid)
+
+			begin
+				if $?.exitstatus == 0
+					input = IO.read(tmpfile.path)
+				else
+					raise "Cancelled"
+				end
+			ensure
+				tmpfile.close
+				tmpfile.unlink
+			end
+
+			input
+		end
+	end
 end
